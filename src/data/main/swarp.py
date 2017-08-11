798f0c133e2b94eb15827206432e9b5136ce55f1
Mehnaaz Asad <asadm2@as05022.local>
2017-08-10 23:47:56 -0500

    First commit

---
+++ src/data/main/swarp.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 16:39:59 2017
+
+@author: asadm2
+"""
+from __future__ import division, print_function, absolute_import
+import astropy
+from glob import glob
+from astropy.io import fits
+from astropy.io.fits import getdata
+import warnings
+from astropy.utils.exceptions import AstropyUserWarning
+import pandas as pd
+import numpy as np
+import os
+import subprocess
+import imp
+
+foo = imp.load_source('ECO_funcs', '../mods_swarp/ECO_funcs.py')
+
+#Formatted ECO catalog with filters and new_filename column and list of 
+#good ECO objects
+ECOnew  = '../../../data/interim/ECO_formatted.txt'
+goodObj = '../../../data/interim/goodObj.txt'
+
+#Using the make_dict function to return a dictionary whose keys are ECOID and
+#filter name and whose values are filenames, return the list of sorted keys,
+#and the formatted ECO catalog
+ECO_dict,ECO_keys,ECOcat = foo.make_dict(ECOnew,goodObj)
+
+for index,key in enumerate(ECO_keys):
+    print('Key {0}/{1}'.format(index+1,len(ECO_keys)))
+    obj = key[0]
+    filter_i = key[1]
+    dir_path = os.getcwd()
+    #Since this file is in src/ and we need to access the images we have to 
+    #move to the data/raw folder the first time this code runs
+    if os.path.basename(dir_path) == 'main':
+        os.chdir('../../../data/raw/'+obj)
+    #After the first time all that has to be done is to move out of the  
+    #previous ECO object dir and into the next one
+    elif os.path.basename(dir_path) != obj:
+        os.chdir('..')
+        os.chdir(obj)
+    #Using the ECOID,filter key to acess the values in the new_filename column
+    #i.e. get a list of the images associated with this key pair
+    imgs = ECO_dict.get_group(key)['new_filename'].values
+    #Using the update_header function to output the name of the text file
+    #that contains the names of the new versions of the fits images being
+    #input into the function
+    img_list_txt = foo.update_header(imgs,obj,filter_i)
+
+    #to check for case where all images were in Except clause of update_header
+    #and only run swarp if there are at least 2 images (NEED TO CHANGE)
+    if len(imgs) >= 2 and img_list_txt != 'error': 
+        ra = np.unique(ECOcat.RA.loc[ECOcat.ECOID==obj])[0]
+        dec = np.unique(ECOcat.DEC.loc[ECOcat.ECOID==obj])[0]
+        
+        #Running swarp with parameters. The only ones that change are the list
+        #of images, the RA and DEC of the object, the object name and the 
+        #filter name
+        print('Starting to swarp')    
+        subprocess.call(['swarp','@'+img_list_txt,'-CENTER_TYPE','MANUAL', \
+        '-CENTER',str(ra)+","+str(dec),'-NTHREADS','32', \
+        '-COMBINE_BUFSIZE','10000','-MEM_MAX','1000','-VMEM_MAX','5000',\
+        '-IMAGE_SIZE','3960','-GAIN_KEYWORD','CCDGAIN','-GAIN_DEFAULT','1.0',\
+        '-SUBTRACT_BACK','Y','-IMAGEOUT_NAME',obj+'_'+filter_i+'_'+'coadd.fits'])
+        print('Finished swarping')
+        
+        coadd = fits.open(obj+'_'+filter_i+'_'+'coadd.fits')
+        #Open the coadd and use the percent_blank function 
+        foo.percent_blank(coadd,obj,filter_i)
+        
+    #If the length of imgs is 1 then check if the new version that 
+    #update_header function creates exists. If it does then calculate blank
+    #percentage. If the length is 0 i.e. since the original ECO catalog is 
+    #being used some ECOID, filter pairs no longer exist since they were
+    #eliminated in preliminary exptime and >2filter checks so for those pairs
+    #the files won't exist. They are ignored which is why percent_blank doesn't
+    #print anything to the terminal.
+    else:
+        filename = imgs[0]+'_test_'+filter_i+'.fits'
+        if os.path.exists(filename): #avoid empty cases where files don't exist 
+            single_img = fits.open(filename)
+            foo.percent_blank(single_img,obj,filter_i)           
