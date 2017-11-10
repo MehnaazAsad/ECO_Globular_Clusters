e5d30355c25eed99bfe62eea42c659869648d489
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2017-11-10 02:32:42 -0600

    Repeating Expfil2 because of error in ECO00026

--- src/data/main/Obj_in_Img.py
+++ src/data/main/Obj_in_Img.py
@@ -90,7 +90,7 @@ for index,obj in enumerate(arr_goodObj):
 #    print(final_good_img_arr)
 #    print(np.ravel(final_good_img_arr))
     
-    final_good_img_num = len(np.array(final_good_img_arr).flatten())
+    final_good_img_num = np.array(final_good_img_arr).size
     
     if len(ECO_match) >= 2:
         filter_num = len(ECO_match)
