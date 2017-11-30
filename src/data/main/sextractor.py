1c4a265dfde8917357ff055dc3beb66866d3d71d
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2017-11-30 00:15:29 -0600

    Running again for goodObjv2

--- src/data/main/sextractor.py
+++ src/data/main/sextractor.py
@@ -146,7 +146,7 @@ plt.scatter(x,sdssr_cat, c='g',label='catalog rmag')
 plt.xlabel('ECOID')
 plt.ylabel('rmag')
 plt.gca().invert_yaxis()
-plt.legend(loc='best')
+plt.legend(loc='lower right')
 plt.title('Comparison of calculated rmag and rmag from ECO photometric table')
 plt.savefig('calcrmag_catrmag.png')
     
