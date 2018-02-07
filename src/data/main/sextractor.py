13268f5332ea05a9b46e991d041b40b2e52caa15
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2018-02-07 02:17:29 -0600

    Plotting post processed ECO00026 rmag in red

--- src/data/main/sextractor.py
+++ src/data/main/sextractor.py
@@ -43,6 +43,7 @@ ECO_phot_cat  = pd.read_csv(ECOphotcat, delim_whitespace=True,header=None,\
 sdssr_stpetro_calc = []
 sdssr_abpetro_calc = []
 sdssi_stpetro_calc = []
+good_Obj_subset = []
 #sdssr_iso_calc = []
 #sdssr_isocorr_calc = []
 #sdssr_auto_calc = []
@@ -125,35 +126,35 @@ for index,obj in enumerate(good_Obj.ECOID):
     
     print('Getting petro mag from test.cat')
 
-    try:
-        cat_sextractor = SkyCoord(f814w_cat['x_world']*u.deg,\
-                                  f814w_cat['y_world']*u.deg)
-        cat_eco = SkyCoord(ra*u.deg, dec*u.deg)
-        idx_sdss, d2d_sdss, d3d_sdss = cat_eco.match_to_catalog_sky(cat_sextractor)
-    
-        f814mag = f814w_cat.petro_mag.values[idx_sdss]
-        
-        if f814mag == 99.0:
-            raise ValueError
-        
-    except ValueError as valueerror:
-        os.chdir('..')
-        with open('magnitude_errors_catmatch.txt','a') as newfile:
-            newfile.write('{0} has a mag of 99\n'.format(obj))
-            newfile.close()
-        print('Magnitude is 99. Moving to next object.')
-        os.chdir(obj)
-        hdu_f814w_coadd.close()
-        continue
-    
-    print('Writing separation to file')
-    print(d2d_sdss)
-    os.chdir('..')
-    with open('catmatch_separation.txt','a') as newfile:
-        newfile.write('Separation for {0} is: {1}'.format(obj,d2d_sdss.arcsec[0]))
-        newfile.write('\n')
-        newfile.close()
-    os.chdir(obj)
+#    try:
+#        cat_sextractor = SkyCoord(f814w_cat['x_world']*u.deg,\
+#                                  f814w_cat['y_world']*u.deg)
+#        cat_eco = SkyCoord(ra*u.deg, dec*u.deg)
+#        idx_sdss, d2d_sdss, d3d_sdss = cat_eco.match_to_catalog_sky(cat_sextractor)
+#    
+#        f814mag = f814w_cat.petro_mag.values[idx_sdss]
+#        
+#        if f814mag == 99.0:
+#            raise ValueError
+#        
+#    except ValueError as valueerror:
+#        os.chdir('..')
+#        with open('magnitude_errors_catmatch.txt','a') as newfile:
+#            newfile.write('{0} has a mag of 99\n'.format(obj))
+#            newfile.close()
+#        print('Magnitude is 99. Moving to next object.')
+#        os.chdir(obj)
+#        hdu_f814w_coadd.close()
+#        continue
+#    
+#    print('Writing separation to file')
+#    print(d2d_sdss)
+#    os.chdir('..')
+#    with open('catmatch_separation.txt','a') as newfile:
+#        newfile.write('Separation for {0} is: {1}'.format(obj,d2d_sdss.arcsec[0]))
+#        newfile.write('\n')
+#        newfile.close()
+#    os.chdir(obj)
         
             
 #    isomag = f814w_cat.iso_mag.values[idx_sdss]
@@ -162,34 +163,34 @@ for index,obj in enumerate(good_Obj.ECOID):
 #    
 #    petroflux = f814w_cat.petro_flux.values[idx_sdss]
     
-#    try:
-#        f814mag = f814w_cat.petro_mag.loc[((f814w_cat.xmin_image < [xx])&([xx] < \
-#                                               f814w_cat.xmax_image))&\
-#                                              ((f814w_cat.ymin_image < [yy])&([yy] < \
-#                                               f814w_cat.ymax_image))]\
-#                                              .values[0] 
-#        if f814mag == 99.0:
-#            raise ValueError
-#    except IndexError as indexerror:
-#        os.chdir('..')
-#        with open('magnitude_errors.txt','a') as newfile:
-#            newfile.write('{0} gives {1}\n'.format(obj,indexerror))
-#            newfile.close()
-#        print('Error in getting magnitude. Moving to next object.')
-#        os.chdir(obj)
-#        hdu_f814w_coadd.close()
-#        continue
-#    except ValueError as valueerror:
-#        os.chdir('..')
-#        with open('magnitude_errors.txt','a') as newfile:
-#            newfile.write('{0} has a mag of 99\n'.format(obj))
-#            newfile.close()
-#        print('Magnitude is 99. Moving to next object.')
-#        os.chdir(obj) #Otherwise line 59 would throw an error
-#        hdu_f814w_coadd.close()
-#        continue
+    try:
+        f814mag = f814w_cat.petro_mag.loc[((f814w_cat.xmin_image < [xx])&([xx] < \
+                                               f814w_cat.xmax_image))&\
+                                              ((f814w_cat.ymin_image < [yy])&([yy] < \
+                                               f814w_cat.ymax_image))]\
+                                              .values[0] 
+        if f814mag == 99.0:
+            raise ValueError
+    except IndexError as indexerror:
+        os.chdir('..')
+        with open('magnitude_errors.txt','a') as newfile:
+            newfile.write('{0} gives {1}\n'.format(obj,indexerror))
+            newfile.close()
+        print('Error in getting magnitude. Moving to next object.')
+        os.chdir(obj)
+        hdu_f814w_coadd.close()
+        continue
+    except ValueError as valueerror:
+        os.chdir('..')
+        with open('magnitude_errors.txt','a') as newfile:
+            newfile.write('{0} has a mag of 99\n'.format(obj))
+            newfile.close()
+        print('Magnitude is 99. Moving to next object.')
+        os.chdir(obj) #Otherwise line 59 would throw an error
+        hdu_f814w_coadd.close()
+        continue
         
-        
+    good_Obj_subset.append(obj)   
                                       
 #    magerr = f814w_cat.petro_magerr.loc[((f814w_cat.xmin_image < [xx])&([xx] <\
 #                                       f814w_cat.xmax_image))&\
@@ -242,18 +243,13 @@ for index,obj in enumerate(good_Obj.ECOID):
 copy_sdssr = sdssr_cat
 copy_sdssi = sdssi_cat
 
-sdss_r_cat = np.array(sdss_r_cat)
-sdss_i_cat = np.array(sdss_i_cat)
-sdssr_stpetro_calc = np.array(sdssr_stpetro_calc)
-sdssi_stpetro_calc = np.array(sdssi_stpetro_calc)
-
 os.chdir('..')
 print('Plotting')
 #x = np.linspace(0,len(good_Obj_subset)+1,len(good_Obj_subset))
 #my_xticks = good_Obj_subset
 fig1 = plt.figure(figsize=(10,8))
 #plt.xticks(x, my_xticks,rotation=90)
-for label,x,y in zip(good_Obj.ECOID,sdss_r_cat,sdssr_stpetro_calc):
+for label,x,y in zip(good_Obj_subset,sdss_r_cat,sdssr_stpetro_calc):
     if label=='ECO00026':
         plt.scatter(x,y,s=50,c='red',label='ECO00026')
     else:
